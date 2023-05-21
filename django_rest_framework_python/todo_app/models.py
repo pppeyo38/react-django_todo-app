63a1f546b8c84c5630d88375f050aa78c0918ea2
pppeyo38 <fuwafuwa.melon47@gmail.com>
2023-05-21 14:19:18 +0900

    :sparkles: add&update application file

--- django_rest_framework_python/todo_app/models.py
+++ django_rest_framework_python/todo_app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class TestData(models.Model):
+    testname = models.CharField(max_length=100)
+    testvalue = models.IntegerField()
--- django_rest_framework_python/todo_app/views.py
+++ django_rest_framework_python/todo_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import TestData
+from .serializers import TestDataSerializer
 
-# Create your views here.
+class TestDataViewSet(viewsets.ModelViewSet):
+    queryset = TestData.objects.all()
+    serializer_class = TestDataSerializer
