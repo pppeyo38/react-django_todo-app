674953d5e6b71998ed7cbf2ab81732837935e4a3
pppeyo38 <fuwafuwa.melon47@gmail.com>
2023-05-21 16:12:04 +0900

    :sparkles: update serializers

--- django_rest_framework_python/todo_app/serializers.py
+++ django_rest_framework_python/todo_app/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-# from .models import TestData
+from .models import User, Todo
 
-# class TestDataSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = TestData
-#         fields = '__all__'
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'
