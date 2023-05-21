95a8ab0eaa49f60528d6a9b85c9a2c4f2bfbeda2
pppeyo38 <fuwafuwa.melon47@gmail.com>
2023-05-21 19:11:00 +0900

    :+1: change urlpatterns

--- django_rest_framework_python/django_rest_framework_python/urls.py
+++ django_rest_framework_python/django_rest_framework_python/urls.py
@@ -5,7 +5,7 @@ from todo_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', views.UserList.as_view()),
-    path('users/<int:pk>', views.UserDetail.as_view()),
-    path('todo_list/', views.TodoList.as_view()),
+    path('user/<int:pk>', views.UserDetail.as_view()),
+    path('todos/', views.TodoList.as_view()),
     path('todo/<int:pk>', views.TodoDetail.as_view()),
 ]
