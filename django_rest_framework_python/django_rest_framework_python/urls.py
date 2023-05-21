b00cae314e897634c3406c7931627d49cadcb83f
pppeyo38 <fuwafuwa.melon47@gmail.com>
2023-05-21 14:27:24 +0900

    :tada: migration & runserver

--- django_rest_framework_python/django_rest_framework_python/urls.py
+++ django_rest_framework_python/django_rest_framework_python/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo_app/v1/', include('apiv1.urls'))
+    path('todo_app/', include('todo_app.urls'))
 ]
