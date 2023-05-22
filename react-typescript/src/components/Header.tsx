5c9d29c77655262fc65a968a1cc47bacf0215fac
pppeyo38 <fuwafuwa.melon47@gmail.com>
2023-05-22 15:02:35 +0900

    :sparkles: add components folder

---
+++ react-typescript/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import '../styles/components/header.scss'
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header-heading">TODO's</h1>
+    </header>
+  )
+}
--- react-typescript/src/pages/TodoList.tsx
+++ react-typescript/src/pages/TodoList.tsx
@@ -1,10 +1,12 @@
 import { useParams } from 'react-router-dom'
+import { Header } from '../components/Header'
 
 export const TodoList = () => {
   const { username } = useParams<{ username: string }>()
 
   return (
     <>
+      <Header />
       <h1>{username}のTODOリスト</h1>
     </>
   )
