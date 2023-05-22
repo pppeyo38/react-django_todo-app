9d25857ef71f704d4deffe51067cd30c7a277b4a
pppeyo38 <fuwafuwa.melon47@gmail.com>
2023-05-22 14:45:05 +0900

    :sparkles: add react-router-dom

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,10 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './router/Router'
 import './styles/App.scss'
 
 function App() {
   return (
-    <>
-      <p className="test-text">Hello, World</p>
-    </>
+    <BrowserRouter basename="/">
+      <Router />
+    </BrowserRouter>
   )
 }
 
